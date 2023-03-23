9fbdf6f6c7dc6322cb4ad6ea6e488e10d9c125c9
Sebastian Mikalsen <sebtmik@gmail.com>
2023-03-23 14:57:48 +0100

    fix: remove uri encoding

--- src/util.ts
+++ src/util.ts
@@ -195,7 +195,6 @@ export function createImage(url: string): Promise<HTMLImageElement> {
 export async function svgToDataURL(svg: SVGElement): Promise<string> {
   return Promise.resolve()
     .then(() => new XMLSerializer().serializeToString(svg))
-    .then(encodeURIComponent)
     .then((html) => `data:image/svg+xml;charset=utf-8,${html}`)
 }
 
